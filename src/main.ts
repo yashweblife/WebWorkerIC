0d5832199b4ea29e28d09f493d0a1e005e271f6d
yashweblife <yashweblife20@gmail.com>
2023-08-11 04:11:19 -0500

    add basic main thread code

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,64 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import { createCanvas } from './helpers';
+import Worker from "./worker1.ts?worker";
+const {canvas, c} = createCanvas();
+const w1 = new Worker();
+const col = [];
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const holder = document.createElement('div');
+document.body.appendChild(holder);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const submitButton = document.createElement('button');
+submitButton.innerText = 'Submit';
+document.body.appendChild(submitButton);
+submitButton.addEventListener('click', () => {
+  if(c){
+    const newCanvas = document.createElement('canvas');
+    newCanvas.width = 100;
+    newCanvas.height = 100;
+    newCanvas.style.border = '1px solid black';
+    const newC = newCanvas.getContext('2d');
+    if(newC){
+      newC.drawImage(canvas,0,0,100,100);
+      holder.appendChild(newCanvas);
+      col.push(newCanvas);
+    }
+    c.clearRect(0,0,300,300);
+  }
+})
+
+const trainButton = document.createElement('button');
+trainButton.innerText = 'Train';
+document.body.appendChild(trainButton);
+trainButton.addEventListener('click', () => {
+  w1.postMessage(c?.getImageData(0,0,300,300));
+})
+
+w1.onmessage = (e:MessageEvent)=>{
+  console.log(11);
+}
+
+const mouse = {
+  pos:{
+    x:0,
+    y:0
+  },
+  clicked:false
+}
+canvas.addEventListener("mousemove",(e:MouseEvent)=>{
+  mouse.pos.x = e.clientX;
+  mouse.pos.y = e.clientY;
+  if(mouse.clicked){
+    if(c){
+      c.beginPath();
+      c.arc(mouse.pos.x,mouse.pos.y,3,0,Math.PI*2,false);
+      c.fill();
+      c.closePath()
+    }
+  }
+});
+canvas.addEventListener("mousedown",()=>{
+  mouse.clicked = true;
+});
+canvas.addEventListener("mouseup",()=>{
+  mouse.clicked = false;
+});
