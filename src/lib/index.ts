1596f6bb2146e2b3d72e7d66e1b1718b211fd961
yashweblife <yashweblife20@gmail.com>
2023-08-11 05:06:31 -0500

    add button to export

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+import { Button } from './Button/Button';
+import { Canvas } from './Canvas/Canvas';
+import { Mouse } from './Mouse/Mouse';
+import { Vector } from './Vector/Vector';
+export { Button, Canvas, Mouse, Vector };
+
