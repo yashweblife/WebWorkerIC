b84cc9e6bd7b7fb204bd44794faa3759292f4d71
yashweblife <yashweblife20@gmail.com>
2023-08-11 05:12:57 -0500

    reset worker1

--- src/worker1.ts
+++ src/worker1.ts
@@ -1,15 +1,3 @@
 onmessage = (e:MessageEvent)=>{
-    console.log("HELLO")
-    // const data = e.data;
-    // console.log(data);
-    // const output = []
-    // for(let i=0;i<data.length;i+=4){
-    //     const avg = (data[i]+data[i+1]+data[i+2])/3;
-    //     if(avg<128){
-    //         output.push(1);
-    //     }else{
-    //         output.push(0);
-    //     }
-    // }
-    self.postMessage("output");
+
 }
