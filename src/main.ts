6cd15d5ac4d4d0658a92cb2a3698e95a0cd7070c
yashweblife <yashweblife20@gmail.com>
2023-08-11 05:05:43 -0500

    add basic working interface

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,40 @@
-import { createCanvas } from './helpers';
-import Worker from "./worker1.ts?worker";
-const {canvas, c} = createCanvas();
-const w1 = new Worker();
-const col = [];
+import { Button, Canvas, Mouse, Vector } from "./lib";
 
-const holder = document.createElement('div');
-document.body.appendChild(holder);
-
-const submitButton = document.createElement('button');
-submitButton.innerText = 'Submit';
-document.body.appendChild(submitButton);
-submitButton.addEventListener('click', () => {
-  if(c){
-    const newCanvas = document.createElement('canvas');
-    newCanvas.width = 100;
-    newCanvas.height = 100;
-    newCanvas.style.border = '1px solid black';
-    const newC = newCanvas.getContext('2d');
-    if(newC){
-      newC.drawImage(canvas,0,0,100,100);
-      holder.appendChild(newCanvas);
-      col.push(newCanvas);
-    }
-    c.clearRect(0,0,300,300);
-  }
+let points:Vector[] = []
+const canvas = new Canvas();
+const mouse = new Mouse();
+const clearCanvasButton = new Button("Clear Canvas");
+clearCanvasButton.addEvent("click",()=>{
+    canvas.clear();
 })
-
-const trainButton = document.createElement('button');
-trainButton.innerText = 'Train';
-document.body.appendChild(trainButton);
-trainButton.addEventListener('click', () => {
-  w1.postMessage(c?.getImageData(0,0,300,300));
+const submitButton = new Button("Hello");
+submitButton.addEvent("click",()=>{
+    const newCanvas = new Canvas(new Vector(100,100));
+    newCanvas.dom.style.border = "1px solid black";
+    newCanvas.drawImage(canvas.dom);
+    newCanvas.addEvent("click",()=>{
+        canvas.drawImage(newCanvas.dom);
+    })
+    newCanvas.addEvent("dblclick",()=>{
+        newCanvas.dom.remove();
+    })
 })
-
-w1.onmessage = (e:MessageEvent)=>{
-  console.log(11);
-}
-
-const mouse = {
-  pos:{
-    x:0,
-    y:0
-  },
-  clicked:false
-}
-canvas.addEventListener("mousemove",(e:MouseEvent)=>{
-  mouse.pos.x = e.clientX;
-  mouse.pos.y = e.clientY;
-  if(mouse.clicked){
-    if(c){
-      c.beginPath();
-      c.arc(mouse.pos.x,mouse.pos.y,3,0,Math.PI*2,false);
-      c.fill();
-      c.closePath()
+canvas.addEvent("mousemove",(e:MouseEvent)=>{
+    mouse.pos.x = e.clientX;
+    mouse.pos.y = e.clientY;
+    if(mouse.clicked){
+        points.push(new Vector(mouse.pos.x,mouse.pos.y));
+        canvas.arc(mouse.pos,3,"black");
+    }else{
+        points = [];
     }
-  }
-});
-canvas.addEventListener("mousedown",()=>{
-  mouse.clicked = true;
-});
-canvas.addEventListener("mouseup",()=>{
-  mouse.clicked = false;
-});
+})
+document.body.addEventListener("mousedown",(e:MouseEvent)=>{
+    mouse.clicked = true;
+})
+document.addEventListener("mouseup",(e:MouseEvent)=>{
+    for(let i=0;i<points.length-1;i++){
+        canvas.line(points[i],points[i+1]);
+    } 
+    mouse.clicked = false;
+})
