f93afd4982cee6e60319aa942520545d824c30da
yashweblife <yashweblife20@gmail.com>
2023-08-17 23:08:22 -0500

    new stuff

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 import { Button } from './Button/Button';
 import { Canvas } from './Canvas/Canvas';
+import { Matrix, Matrix2D, Matrix2DVector } from './Matrix/Matrix';
 import { Mouse } from './Mouse/Mouse';
-import { Vector } from './Vector/Vector';
-export { Button, Canvas, Mouse, Vector };
+import { Vector, VectorMath } from './Vector/Vector';
+export { Button, Canvas, Matrix, Matrix2D, Matrix2DVector, Mouse, Vector, VectorMath };
 
